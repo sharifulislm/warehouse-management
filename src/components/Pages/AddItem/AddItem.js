04b72a51482020dcc2f85f4ad3595805e4ec11da
sharifulislm <shariful.islam.coder@gmail.com>
2022-05-07 01:31:58 +0300

    added post server to ui

--- src/components/Pages/AddItem/AddItem.js
+++ src/components/Pages/AddItem/AddItem.js
@@ -3,16 +3,35 @@ import { useForm } from "react-hook-form";
 
 const AddItem = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+     console.log(data);
+     const url = `http://localhost:5000/inventory`;
+     fetch(url, {
+         method: 'POST',
+         headers: {
+             'content-type': 'application/json'
+         },
+         body: JSON.stringify(data)
+     })
+     .then(res => res.json())
+     .then(result => {
+         console.log(result);
+     })
+    };
+
     return (
-        <div>
+        <div className='container'>
             <h2>Please add a service</h2>
-           <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName", { required: true, maxLength: 20 })} />
-      <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-      <input type="number" {...register("age", { min: 18, max: 99 })} />
-      <input type="submit" />
+            <div className='w-50 mx-auto form-box'>
+           <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+      <input className=' mb-3 ' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
+      <input className='mb-2' placeholder='supplier ' {...register("supplierName")} />
+      <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
+      <input className='mb-2' placeholder='Photo url' type="text" {...register("images")} />
+      <textarea className='mb-2' placeholder='Description' {...register("description")} />
+      <input type="submit" value="Add Item"/>
     </form>
+        </div>
         </div>
     );
 };
